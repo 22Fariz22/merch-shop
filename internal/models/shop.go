be8e2372fe1366452b4b3006a7095286cff974b5
22fariz22 <fariz08@gmail.com>
2025-03-25 22:03:45 +0300

    Add models

--- internal/models/shop.go
+++ internal/models/shop.go
@@ -1,4 +1,3 @@
 package models
 
-type User struct{}
 type Shop struct{}
---
+++ internal/models/user.go
@@ -0,0 +1,58 @@
+package models
+
+import (
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// User model
+type User struct {
+	UserID    uuid.UUID `json:"user_id" db:"user_id" redis:"user_id" validate:"omitempty"`
+	Username  string    `json:"username" db:"username" redis:"username" validate:"required,lte=30"`
+	Password  string    `json:"password,omitempty" db:"password" redis:"password" validate:"omitempty,required,gte=6"`
+	CreatedAt time.Time `json:"created_at,omitempty" db:"created_at" redis:"created_at"`
+}
+
+// Find user
+type UserWithToken struct {
+	User  *User  `json:"user"`
+	Token string `json:"token"`
+}
+
+// Prepare user for register
+func (u *User) PrepareCreate() error {
+	u.Username = strings.ToLower(strings.TrimSpace(u.Username))
+	u.Password = strings.TrimSpace(u.Password)
+
+	if err := u.HashPassword(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Hash user password with bcrypt
+func (u *User) HashPassword() error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+	return nil
+}
+
+// Sanitize user password
+func (u *User) SanitizePassword() {
+	u.Password = ""
+}
+
+// Compare user password and payload
+func (u *User) ComparePasswords(password string) error {
+	if err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password)); err != nil {
+		return err
+	}
+	return nil
+}
